b6199a138cca3532e69a990737858fbd5dfd80e6
Pedro Quidiante <pedroalejandro.quidiante@alumnos.ulagos.cl>
2025-12-06 23:32:52 -0300

    c-3

--- src/lib/conections.ts
+++ src/lib/conections.ts
@@ -1,17 +1,20 @@
+// DECLARE UNA VARIABLE DE TIPO CONST PARA CONSUMIR LA URL DE STRAPI
+
 const strapiUrl = import.meta.env.PUBLIC_STRAPI_URL || 'https://astro-backend-yryd.onrender.com/api';
 
+// La vamos usar para las images
 export const STRAPI_BASE_URL = strapiUrl.replace('/api', '');
-export const STRAPI_URL = strapiUrl;
+export const PUBLIC_STRAPI_URL = strapiUrl;
 
 export const fetchFromAPI = async (slug: string) => {
     try {
-        const response = await fetch(`${STRAPI_URL}/${slug}?populate=*`);
+        const response = await fetch(`${PUBLIC_STRAPI_URL}/${slug}?populate=*`);
         const json = await response.json();
 
         if(!response.ok) {
             throw new Error('El error', json.message);
         }
-
+        // Si tiene results, es una lista, si no, es un objeto individual
         return json.data;
     } catch (error) {
         console.error(error);
